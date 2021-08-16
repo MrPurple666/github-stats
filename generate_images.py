7a4cd950467b4c821c835c066481e0fe2f15f93c
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-08-16 02:34:39 -0300

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -64,12 +64,12 @@ async def generate_languages(s: Stats) -> None:
     for i, (lang, data) in enumerate(sorted_languages):
         color = data.get("color")
         color = color if color is not None else "#000000"
-        progress += (f'<span style="background-color: {FF000000};'
+        progress += (f'<span style="background-color: {color};'
                      f'width: {data.get("prop", 0):0.3f}%;" '
                      f'class="progress-item"></span>')
         lang_list += f"""
 <li style="animation-delay: {i * delay_between}ms;">
-<svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:{FF7D20C6};"
+<svg xmlns="http://www.w3.org/2000/svg" class="octicon" style="fill:{color};"
 viewBox="0 0 16 16" version="1.1" width="16" height="16"><path
 fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">{lang}</span>
